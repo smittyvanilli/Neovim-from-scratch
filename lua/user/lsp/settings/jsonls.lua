7c93a83ed7c4691be207d43d15e6ec6cc2ddb210
smittyvanilli <stephenmilley@gmail.com>
2022-09-04 17:19:51 -0400

    update jsonls and delete lsp setting pyright LUA file

--- lua/user/lsp/settings/jsonls.lua
+++ lua/user/lsp/settings/jsonls.lua
@@ -1,3 +1,9 @@
+local default_schemas = nil
+local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
+if status_ok then
+  default_schemas = jsonls_settings.get_default_schemas()
+end
+
 local schemas = {
   {
     description = "TypeScript compiler configuration file",
@@ -162,10 +168,19 @@ local schemas = {
   },
 }
 
+local function extend(tab1, tab2)
+  for _, value in ipairs(tab2) do
+    table.insert(tab1, value)
+  end
+  return tab1
+end
+
+local extended_schemas = extend(schemas, default_schemas)
+
 local opts = {
   settings = {
     json = {
-      schemas = schemas,
+      schemas = extended_schemas,
     },
   },
   setup = {

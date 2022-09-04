6a00c8776f332083684474adced7d8f017f0289b
smittyvanilli <stephenmilley@gmail.com>
2022-09-04 17:23:06 -0400

    update packer_compiled LUA file for gitsigns plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -119,6 +119,11 @@ _G.packer_plugins = {
     path = "/home/smitty/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["gitsigns.nvim"] = {
+    loaded = true,
+    path = "/home/smitty/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    url = "https://github.com/lewis6991/gitsigns.nvim"
+  },
   ["nlsp-settings.nvim"] = {
     loaded = true,
     path = "/home/smitty/.local/share/nvim/site/pack/packer/start/nlsp-settings.nvim",

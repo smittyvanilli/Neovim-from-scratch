f35e59424599d830ffdeaab24a03c4c78dae7768
smittyvanilli <stephenmilley@gmail.com>
2022-09-06 03:20:08 -0400

    modify plugin packer_compiler LUA file for togglterm plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -204,6 +204,11 @@ _G.packer_plugins = {
     path = "/home/smitty/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["toggleterm.nvim"] = {
+    loaded = true,
+    path = "/home/smitty/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
+  },
   ["vim-bbye"] = {
     loaded = true,
     path = "/home/smitty/.local/share/nvim/site/pack/packer/start/vim-bbye",

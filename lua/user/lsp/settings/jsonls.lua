b450d590903d738c68c3cc23dee9d56b19d2313c
smittyvanilli <stephenmilley@gmail.com>
2022-09-11 08:11:12 -0400

    fix ipairs function in user settings jsonls LUA file

--- lua/user/lsp/settings/jsonls.lua
+++ lua/user/lsp/settings/jsonls.lua
@@ -169,7 +169,10 @@ local schemas = {
 }
 
 local function extend(tab1, tab2)
-  for _, value in ipairs(tab2 or {}) do
+  if tab2 == nil then
+    return tab2
+  end
+  for _, value in ipairs(tab2) do
     table.insert(tab1, value)
   end
   return tab1

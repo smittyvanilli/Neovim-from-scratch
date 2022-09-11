da1ca63e50a588559a71b6ff62b85ba5331bc66c
smittyvanilli <stephenmilley@gmail.com>
2022-09-11 05:55:32 -0400

    update plugin packer_compiler LUA file for plugin whichkey

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -238,6 +238,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/smitty/.local/share/nvim/site/pack/packer/start/vim-bbye",
     url = "https://github.com/moll/vim-bbye"
+  },
+  ["which-key.nvim"] = {
+    loaded = true,
+    path = "/home/smitty/.local/share/nvim/site/pack/packer/start/which-key.nvim",
+    url = "https://github.com/folke/which-key.nvim"
   }
 }
 

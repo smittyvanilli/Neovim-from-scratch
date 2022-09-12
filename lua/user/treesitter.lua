6669752ff6f55dbc07b3c355424c49c0bce049c0
smittyvanilli <stephenmilley@gmail.com>
2022-09-12 08:54:53 -0400

    add rainbow function to treesitter LUA file

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -22,5 +22,13 @@ configs.setup {
   },
   autotag = {
     enable = true,
+  },
+  rainbow = {
+    enable = true,
+    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+    extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
   }
 }

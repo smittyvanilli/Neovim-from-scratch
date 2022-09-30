acd119afe1a324bd12e56fe947b3f46ecc38bc62
Stephen Milley <stephenmilley@gmail.com>
2022-09-30 18:23:48 -0400

    feat(mason.lua) create mason plugin in lua user

---
+++ lua/user/mason.lua
@@ -0,0 +1,13 @@
+require("mason").setup({
+    ui = {
+          border = {"┏", "━", "┓", "┃", "┛", "━", "┗", "┃"},
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+      }
+})
+    }
+  }
+})

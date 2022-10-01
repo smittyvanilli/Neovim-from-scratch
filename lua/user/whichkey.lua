6cd2648a5ff45f10f659862e37cd8faf1cdf3b02
Stephen Milley <stephenmilley@gmail.com>
2022-10-01 15:37:06 -0400

    revert(whickey) change IT shortcut to G - otherwise conflicts

--- lua/user/whichkey.lua
+++ lua/user/whichkey.lua
@@ -109,7 +109,7 @@ local mappings = {
     u = { "<cmd>PackerUpdate<cr>", "Update" },
   },
 
-  it = {
+  g = {
     name = "Git",
     g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },

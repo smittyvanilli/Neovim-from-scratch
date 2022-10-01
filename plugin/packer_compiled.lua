5be7fdf5904c67e445fee952f1dfd62aafa81c63
Stephen Milley <stephenmilley@gmail.com>
2022-09-30 21:52:02 -0400

    feat(plugins.lua) add mason and comment out nvim-lsp-install LUA files

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -154,6 +154,11 @@ _G.packer_plugins = {
     path = "/home/smittyvanilli/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
+  ["mason.nvim"] = {
+    loaded = true,
+    path = "/home/smittyvanilli/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    url = "https://github.com/williamboman/mason.nvim"
+  },
   ["nlsp-settings.nvim"] = {
     loaded = true,
     path = "/home/smittyvanilli/.local/share/nvim/site/pack/packer/start/nlsp-settings.nvim",
@@ -173,11 +178,6 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/smittyvanilli/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
-  },
-  ["nvim-lsp-installer"] = {
-    loaded = true,
-    path = "/home/smittyvanilli/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer",
-    url = "https://github.com/williamboman/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
